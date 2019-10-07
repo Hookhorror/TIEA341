001be43474c8c22bc0f1e638a287c930794af6ff
Joni Kiviluoto <joni.kiviluoto@gmail.com>
2019-10-07 12:59:19 +0300

    EXBuildingData
    mkVEctoriin asti hommat tehty

--- EXBuildingData/src/Main.hs
+++ EXBuildingData/src/Main.hs
@@ -1,33 +1,44 @@
 module Main where
 
-newtype Meter = M Double deriving(Eq,Ord,Show)
+
+
+newtype Meter = M Double 
+  deriving (Eq,Ord,Show)
+
+type Length = Int
+
+data Vector2 a = V2 a a 
+  deriving (Eq,Show)
+
+data OneOrTwo a b = This a | That b | These a b
+  deriving (Eq,Show)
+
+data Submission 
+  = S {student :: String, content :: String, date :: (Int,Int,Int)}
+    deriving (Eq,Show)
+
 
 asMeters :: Double -> Meter
-asMeters n = M n
+asMeters x = M x
 
 fromMeters :: Meter -> Double
-fromMeters (M n) = n
-
-
-newtype Length = L Int deriving(Eq,Ord,Show)
+fromMeters (M x) = x
 
 asLength :: Int -> Length
-asLength n = L n
+asLength x = x
+
+mkVector :: Int -> Int -> Vector2 Int
+mkVector x y = V2 x y
+
+-- combine :: Vector2 Int -> Maybe (Vector2 Int) -> OneOrTwo Int Bool
 
 
-newtype Vector2 = V2 Int deriving(Eq,Ord,Show)
-
-mkVector :: Int -> Int -> Vector2
-mkVector n m = V2 (n * m)
-
-
---combine :: Vector2 Int -> Maybe (Vector2 Int) -> OneOrTwo Int Bool
-
---combine3 :: Vector2 Int -> Maybe Bool -> Maybe String 
+-- combine3 :: Vector2 Int -> Maybe Bool -> Maybe String 
+--              -> OneOrTwo Int (OneOrTwo Bool String)
 
 -- submitDay :: Submission -> Int
 
--- getOther :: OneOrTwo a b -> Maybe b
+-- getOther :: OneOrTwo a b -> Maybe b 
 
 
 main :: IO ()
