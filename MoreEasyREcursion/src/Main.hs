977c68c8eb95114f91294b5d22fb7e75397d0e6f
Joni Kiviluoto <joni.kiviluoto@gmail.com>
2019-10-17 14:39:47 +0300

    takeEvens, (+++), myConcat toimii, luulisin
    delete puuttuu vielä

--- MoreEasyREcursion/src/Main.hs
+++ MoreEasyREcursion/src/Main.hs
@@ -1,5 +1,27 @@
 module Main where
 
-main :: IO ()
-main = do
-  putStrLn "hello world"
+  -- delete :: Int -> [Int] -> [Int]
+  -- delete x [] = []
+  -- -- delete 0 [x] = [x]
+  -- -- delete 1 (x:xs) = xs
+  -- delete y (x:xs) = if (y == x) then xs else delete y (x:xs)
+  
+  takeEvens :: [Int] -> [Int]
+  takeEvens [] = []
+  takeEvens (x:xs) = if ((even x) == True) then (x:(takeEvens xs)) else takeEvens xs
+
+  (+++) :: [x] -> [x] -> [x]
+  [] +++ [] = []
+  [] +++ xs = xs
+  (x:xs) +++ ys = x : (xs +++ ys)
+  
+  myConcat :: [[a]] -> [a]
+  myConcat [] = []
+  myConcat xs = case xs of
+    [[]] -> []    
+    [b] -> b
+    (x:ys) -> x ++ (myConcat ys)
+  
+  main :: IO ()
+  main = do
+    putStrLn "hello world"
